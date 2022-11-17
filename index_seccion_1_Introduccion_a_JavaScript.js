e7d21392fb3d0c6ae22f52d96ba2cc15a4501e09
unknown <loctavio.aguilars@gmail.com>
2022-11-17 14:39:00 -0600

    VERSIONES_04_05_06_Instalacion_de_Herramientas_para_JavaScript

--- index_seccion_1_Introduccion_a_JavaScript.js
+++ index_seccion_1_Introduccion_a_JavaScript.js
@@ -1,15 +1,18 @@
 /*
     Programador: Luis Octavio Aguilar Serna
     Curso: JavaScript UDEMY
-    Version (commit): VERSION_03_Recorrido_por_la _Plataforma_de_UDEMY
+    Version (commit): 
+    
     Branch: Master
     
-    SECCION: 1 Introduccion a JavaScript
-    SUBSECCION: 3. Recorrido por la Plataforma de UDEMY
-*/
+    SECCION: 1 Introduccion a JavaScripte
+    SUBSECCIONES: 4. Instalacion de Visual Studio Code
+                  5. Instalcion de Node.js              
+                  6. Extenciones de de Visual Studio Code para JavaScript
+*/ 
 
 
 /*
-    En esta seccion se hace un recorrido a la plataforma de UDEMY, es muy innecesario el commit pero lo hago por cuestiones de
-    organizacion (creo que asi pasara con varios commits).   
+    En las subsecciones indicadas (hago un solo commit para estas subsecciones para ahorrar tiempo) se instalan las herramientas
+    necesarias para trabajar con JavaScript
 */
