6dc1e327b1bcd39fca2475bb5bffd121ded35a2e
unknown <loctavio.aguilars@gmail.com>
2022-11-17 14:25:32 -0600

    VERSION_03_Recorrido_por_la _Plataforma_de_UDEMY

--- index_seccion_1_Introduccion_a_JavaScript.js
+++ index_seccion_1_Introduccion_a_JavaScript.js
@@ -1,17 +1,15 @@
 /*
     Programador: Luis Octavio Aguilar Serna
     Curso: JavaScript UDEMY
-    Version (commit): VERSION_02_Recomendacion_Antes_de_Continuar
+    Version (commit): VERSION_03_Recorrido_por_la _Plataforma_de_UDEMY
     Branch: Master
     
     SECCION: 1 Introduccion a JavaScript
-    SUBSECCION: 2. Recomendacion Antes de Continuar
+    SUBSECCION: 3. Recorrido por la Plataforma de UDEMY
 */
 
 
 /*
-    En esta seccion se hace una recomendacion para estudiar Logica de Programacion, pero ya lo he estudiado previamente en la 
-    escuela.
-
-    Observacion: notese que el nombre del commit siempre va a coincidir con el nombre de la subseccion.
+    En esta seccion se hace un recorrido a la plataforma de UDEMY, es muy innecesario el commit pero lo hago por cuestiones de
+    organizacion (creo que asi pasara con varios commits).   
 */
