6ceb2d0ce76e8b238d094b40be857c884e988314
unknown <loctavio.aguilars@gmail.com>
2022-11-17 14:20:17 -0600

    VERSION_02_Recomendacion_Antes_de_Continuar

--- index_seccion_1_Introduccion_a_JavaScript.js
+++ index_seccion_1_Introduccion_a_JavaScript.js
@@ -1,21 +1,17 @@
 /*
     Programador: Luis Octavio Aguilar Serna
     Curso: JavaScript UDEMY
-    Version (commit): VERSION_01_Introduccion_a_JavaScript
+    Version (commit): VERSION_02_Recomendacion_Antes_de_Continuar
     Branch: Master
     
     SECCION: 1 Introduccion a JavaScript
-    SUBSECCION: 1. Introduccion a JavaScript
+    SUBSECCION: 2. Recomendacion Antes de Continuar
 */
 
 
 /*
-    Introduccion
-    JavaScript es un lenguaje de interpretado, ¿que es un lenjuage de interpretado? Es un lenguaje cuyo codigo principal ejecuta
-    sus instrucciones directamente sin la necesidad de un compilador. JavaScript requiere de un navegador para ser ejecutado 
-    por ende, un navegador no es un interprete. Es uno de los lenguajes mas utilizados en los navegadores. JavaScript es muy
-    utilizado para el desarrollo web "FrontEnd", pero tambien es utilizado en la parte del "BackEnd" debido al avance tecnologico
-    web. Se puede trabajar JavaScript con la tecnologia "Node JS" en la parte del "BackEnd". JavaScript maneja frameworks como
-    Angular, React y Vue.js. JavaScrip trabaja bajo las especificaciones de ECMAscript, la cual se encarga de actualizar cada año
-    a tal lenguaje con el fin de mejorarlo, por esto, esta en constante evolucion.
+    En esta seccion se hace una recomendacion para estudiar Logica de Programacion, pero ya lo he estudiado previamente en la 
+    escuela.
+
+    Observacion: notese que el nombre del commit siempre va a coincidir con el nombre de la subseccion.
 */
