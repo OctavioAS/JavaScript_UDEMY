d15aafcb9ed12954f4ed15ea79e9f118e9c82f27
unknown <loctavio.aguilars@gmail.com>
2022-11-22 10:57:44 -0600

    VERSION_07_Hola_Mundo_con_JavaScript

--- index_seccion_1_Introduccion_a_JavaScript.js
+++ index_seccion_1_Introduccion_a_JavaScript.js
@@ -1,18 +1,39 @@
 /*
     Programador: Luis Octavio Aguilar Serna
     Curso: JavaScript UDEMY
-    Version (commit): 
+    Version (commit): VERSION_07_Hola_Mundo_con_JavaScript
     
     Branch: Master
     
-    SECCION: 1 Introduccion a JavaScripte
-    SUBSECCIONES: 4. Instalacion de Visual Studio Code
-                  5. Instalcion de Node.js              
-                  6. Extenciones de de Visual Studio Code para JavaScript
+    SECCION: 1 Introduccion a JavaScript
+    SUBSECCION: 7 Hola Mundo con JavaScript
 */ 
 
 
 /*
-    En las subsecciones indicadas (hago un solo commit para estas subsecciones para ahorrar tiempo) se instalan las herramientas
-    necesarias para trabajar con JavaScript
+    Primero, definimos una variable. Las variables nos permiten almacenar datos en nuestros programas y tenemos diferentes tipos
+    de datos que puede almacenar una variable.
 */
+
+var nameOne = "Hola Mundo";
+
+/*
+    El tipo var puede definirse sintacticamente como un tipo de dato GENERAL, ya que permite alojar cualquier tipo de de dato
+    relacionado al valor asignado, por ejemplo, nuetra variable nameOne de tipo var tiene un valor asingando de tipo string, este
+    valor podria haber sido un int o cualquier otro. Mas adelante se vera que las variables defninidas pueden cambiar su valor
+    asignado de forma dinamica con relacion a su tipo.
+
+    A continuacion imprimimos nuestra variable:
+*/
+
+console.log (nomeOne);
+
+/*
+    Para observar los resultados tenemos que generar una pagina HTML para observar los resultados en el navegador, pero por
+    cuestiones practicas podemos utilizar la extension Quokka que instalamos previamente, para elle seguimos la siguiente ruta:
+    ->"ctrl + shit + p";->"Quokka.js: New JavaScript File";->"De forma automatica se abre la consola y comenzamos a escribir en
+    el nuevo archivo que se abrio, se vera el programa ejecutandose en la consola".
+
+    En el proximos video veremos como ejecutar Quokka en un archivo existente para evitar creaciones innecesarias de archivos.
+*/
+
