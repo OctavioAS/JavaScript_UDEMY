98e0b8ca4e05a306150f7e2f5dbc25250cfbc1d4
unknown <loctavio.aguilars@gmail.com>
2022-11-23 11:46:52 -0600

    VERSION_9_Nota_Problemas_con_la_extension_de_Quokka

--- index_seccion_1_Introduccion_a_JavaScript.js
+++ index_seccion_1_Introduccion_a_JavaScript.js
@@ -1,28 +1,21 @@
 /*
     Programador: Luis Octavio Aguilar Serna
     Curso: JavaScript UDEMY
-    Version (commit): VERSION_8_Ejecutar_Quokka_en_un_Archivo_Existente
+    Version (commit): VERSION_9_Nota_Problemas_con_la_extension_de_Quokka
     
     Branch: Master
     
     SECCION: 1 Introduccion a JavaScript
-    SUBSECCION: 8 Ejecutar Quokka en un Archivo Existente
+    SUBSECCION: 9 Nota: Problemas con la extension de Quokka
 */ 
 
-
 /*
-    Trabajaremos con el mismo codigo del commit anterior ya que solo veremos como se ejecuta Quokka:
-*/
-
-var nameOne = "Hola Mundo";
-console.log (nameOne);
-
-/*
-    Para ejecutar Quokka en este mismo archivo, seguimos la siguiente ruta:
-    ->"Ctrl+Shit+P";->"Quokka.js: Toogle (Start/Stop) on Current File";->"Sale la consola y se ejecuta el codigo"
-
-    Para detener Quokka seguimos la siguiente ruta:
-    ->"Ctrl+Shit+P";->"Quokka.js: Toogle (Start/Stop) on Current File";->"Se detiene la ejecucion"
+    Si por alguna razon no funciona la extension de quokka en el equipo, todo el codigo se puede ejecutar directamente desde un
+navegador web, de preferencia google chrome. Una vez en chrome ir a la Seccion 20: Manejo del DOM HTML con JavaScript, de este
+mismo curso, y revisar los primeros videos de esta seccion, con esos videos se vera como pueden agregar codigo JavaScript a una
+pagina HTML y desde alli ejecutar su codigo de la misma manera que con la extension de Quokka, sin embargo la extension de 
+Quokka la hemos utilizado para simplificar este proceso, pero si por alguna razón no funciona se pueden hacer todas las prácticas 
+en una página Web y agregar el tag de script y dentro de este tag agregar el mismo código que se ve en cada lección.
 */
 
 
